0c33872fa18fd5bdb8d26dca3d3be4cc29f5a7e1
onglichang <onglichang@hotmail.com>
2022-01-05 15:47:40 +0800

    Add basic HTML/CSS with js logic

---
+++ logic.js
@@ -0,0 +1,41 @@
+const libContainer = document.querySelector('.lib-container');
+let myLibrary = [];
+
+// Test Code
+let someBook = new Book('Dune', 'Frank Herbert', 500, 'Read');
+let someBook2 = new Book('Dune2', 'Frank Herbert', 501, 'Not Read');
+addBookToLibrary(someBook);
+addBookToLibrary(someBook2);
+bookDisplay();
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages; 
+    this.read = read;
+    this.info = function () {
+        return title + " by " + author + ", " + pages 
+        + " pages, " + read;
+    }
+}
+
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
+}
+
+function bookDisplay() {
+    myLibrary.forEach(x => {
+        let bookCard = document.createElement('div');
+        let bookTitle = document.createElement('p');
+        bookTitle.innerText = x.title;
+        let bookAuthor = document.createElement('p');
+        bookAuthor.innerText = x.author;
+        let bookPages = document.createElement('p');
+        bookPages.innerText = x.pages;
+        let bookRead = document.createElement('button');
+        bookRead.innerText = x.read;
+        
+        bookCard.append(bookTitle, bookAuthor, bookPages, bookRead);
+        libContainer.appendChild(bookCard);
+    })
+}
